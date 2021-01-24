468d52d514dce9ec9aa796db493f93bd51197501
Irina-Blazhevich <blazhevych.iryna@gmail.com>
2021-01-24 14:17:23 +0200

    add complited task

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const refs = {
-  stopBtn: document.querySelector('[data-action="stop"]'),
-  startBtn: document.querySelector('[data-action="start"]'),
-  body: document.querySelector('body'),
-};
-let colorSwt;
-let isActive = false;
-
-function startSwitch() {
-  if (!isActive) {
-    isActive = true;
-    colorSwt = setInterval(() => {
-      refs.body.style.backgroundColor = `${
-        colors[randomIntegerFromInterval(0, 5)]
-      }`;
+import './styles.css';
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = new Date(targetDate);
+  }
+  start() {
+    this.intervalId = setInterval(() => {
+      this.currentTime = Date.now();
+      this.time = this.targetDate - this.currentTime;
+      this.timeCounter();
+      this.markUp();
+      if (this.time < 0) {
+        this.stop();
+        this.time = 0;
+        this.timeCounter(0);
+        this.markUp(0);
+      }
     }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.time = 0;
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  timeCounter() {
+    this.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor(
+      (this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    this.mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
+    this.secs = Math.floor((this.time % (1000 * 60)) / 1000);
+  }
+  markUp() {
+    this.refs = {
+      days: document.querySelector('[data-value=days]'),
+      hours: document.querySelector('[data-value=hours]'),
+      mins: document.querySelector('[data-value=mins]'),
+      secs: document.querySelector('[data-value=secs]'),
+    };
+    this.refs.days.textContent = this.pad(this.days);
+    this.refs.hours.textContent = this.pad(this.hours);
+    this.refs.mins.textContent = this.pad(this.mins);
+    this.refs.secs.textContent = this.pad(this.secs);
   }
 }
 
-function stopSwitch() {
-  clearInterval(colorSwt);
-  isActive = false;
-}
-
-refs.startBtn.addEventListener('click', startSwitch);
-refs.stopBtn.addEventListener('click', stopSwitch);
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: 'Jul 17, 2022',
+});
+countdownTimer.start();
